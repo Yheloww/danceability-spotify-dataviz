9ceee1af546a4e5455f87971ff2f315b8a7f0496
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-06-14 15:34:32 +0200

    d3 weird

--- my-app/src/routes/components/Top.svelte
+++ my-app/src/routes/components/Top.svelte
@@ -1,12 +1,11 @@
 <script>
-    import * as d3 from "d3";
-    import data from "../mac_miller.json";
+    import data from "../mac_miller_simple.json";
 
     let danceability = d3.max(data.map(dat => data.danceability))
-    console.log(data)
+    console.log(data.map(data => data.danceability))
+    console.log(danceability)
 
 </script>
 
 <p>Hello</p>
 
-<h1>{danceability}</h1>
