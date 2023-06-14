c4ac89a1f6a3a143993a4423b37b4d0a29d0e1f9
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-06-14 15:43:55 +0200

    mm

--- my-app/src/routes/components/Top.svelte
+++ my-app/src/routes/components/Top.svelte
@@ -1,5 +1,6 @@
 <script>
     import data from "../mac_miller_simple.json";
+    
 
     let danceability = d3.max(data.map(dat => data.danceability))
     console.log(data.map(data => data.danceability))
