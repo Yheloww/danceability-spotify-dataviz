b5f5b0a331b0943fbffe0c8fa2435e0efcfa3b5c
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-06-14 15:27:54 +0200

    commit

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import * as d3 from "d3"
-    import { onMount } from "svelte";
-    import data from "./mac_miller.json"
-  import { svg_element } from "svelte/internal";
+import { onMount } from "svelte";
+import data from "./mac_miller.json"
+import { svg_element } from "svelte/internal";
+import Top from "./components/Top.svelte";
+
 
 console.log(data);
 
@@ -12,38 +14,4 @@ const scale = d3.scaleLinear()
 
 </script>
 
-
-<div class="test">
-
-    {#each data as dat}
-{#if dat.album_name == "Swimming"}
-<div class="song">
-    <h1>{dat.name}</h1>
-<svg>
-    <circle cx=50 cy=50  r={scale(dat.Audio_features.energy)}></circle>
-    <circle cx=150 cy=50  r={scale(dat.Audio_features.danceability)} fill="aquamarine"></circle>
-
-</svg>
-    <p>{dat.Audio_features.energy}</p>
-</div>
-{/if}
-{/each}
-</div>
-
-
-<style>
-    .test {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 2rem;
-        margin-inline: 2rem;
-        justify-content: space-between;
-        align-content: center;
-    }
-    .song {
-        background-color: beige;
-        padding:1rem;
-        border-radius: 2rem;
-    
-    }
-</style>
+<Top/>
---
+++ my-app/src/routes/components/Top.svelte
@@ -0,0 +1,12 @@
+<script>
+    import * as d3 from "d3";
+    import data from "../mac_miller.json";
+
+    let danceability = d3.max(data.map(dat => data.danceability))
+    console.log(data)
+
+</script>
+
+<p>Hello</p>
+
+<h1>{danceability}</h1>
