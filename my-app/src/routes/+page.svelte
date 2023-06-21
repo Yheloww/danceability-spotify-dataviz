6d480fb9adb49a0ff05cf3cd9500a5d6faab8c23
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-06-21 15:07:06 +0200

    line plot advanceent

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -4,8 +4,12 @@ import data from "./mac_miller_simple.json"
 import Top from "./components/Top.svelte";
 import LinePlot from "./components/LinePlot.svelte";
 import album_data from "./album_macmiller.json"
+import HeatmapPlot from "./components/HeatmapPlot.svelte"
 
 </script>
 
 <!-- <Top  data={data}/> -->
-<LinePlot  album_data={album_data}/>
+<LinePlot  album_data={album_data}
+           title={"parameters evolution over time"}/>
+
+<HeatmapPlot data={data} />
